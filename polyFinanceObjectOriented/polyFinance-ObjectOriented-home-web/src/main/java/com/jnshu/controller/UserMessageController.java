7cd4a9fb9b30409e34569c9b7caaa76b98152647
luojiac <37856938+luojiac@users.noreply.github.com>
2018-07-26 18:06:01 +0800

    1
    1

--- polyFinanceObjectOriented/polyFinance-ObjectOriented-home-web/src/main/java/com/jnshu/controller/UserMessageController.java
+++ polyFinanceObjectOriented/polyFinance-ObjectOriented-home-web/src/main/java/com/jnshu/controller/UserMessageController.java
@@ -1,7 +1,11 @@
 package com.jnshu.controller;
 
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
+
+
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,14 +13,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-//用户消息
+/**
+ * 用户消息
+ * @return
+ */
 @Controller
 @RequestMapping("/a/u")
 public class UserMessageController {
-    //消息列表
+    private static final Logger log= LoggerFactory.getLogger(UserMessageController.class);
+    /**
+     * 用户消息中心
+     * @return 消息列表
+     */
     @GetMapping("/user/userMessage/list/{id}")
     @ResponseBody
     public Object messageUser(@PathVariable(value="id") int id, HttpServletRequest request) throws JSONException {
@@ -24,6 +37,8 @@ public class UserMessageController {
         json.put("code",1);
         json.put("message","ok");
         Map<String, String> map=new HashMap<String, String>();
+
+
         map.put("title","消息1");
         map.put("introduce","这是消息");
         map.put("id","11");
